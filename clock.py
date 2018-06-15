c9159c9b2d1376fda6bda71002e3e695f343f8e2
Renato Orgito <orgito@gmail.com>
2018-06-15 11:57:52 -0300

    add a clock dyno

---
+++ clock.py
@@ -0,0 +1,21 @@
+import os
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from oneforge import SYMBOLS, OneForge
+from pymongo import MongoClient
+
+
+def load_quotes(mongo):
+    oforge = OneForge()
+    quotes = oforge.quotes(SYMBOLS)
+    mongo.tubatrade.quotes.insert_many(quotes)
+    print(f"Updated {len(quotes)} quotes")
+
+
+if __name__ == '__main__':
+    mongouri = os.getenv('TUBATRADE_FOREX_MONGO_URI')
+    client = MongoClient(mongouri)
+
+    sched = BlockingScheduler()
+    sched.add_job(load_quotes, trigger='cron', args=[client], minute='*/5')
+    sched.start()
